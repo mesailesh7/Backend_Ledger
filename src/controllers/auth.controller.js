6564fb85ee0dcc1289e73fdf4eab6e6a72e482f3
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2026-03-13 11:48:38 -0600

    authController InProgress

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,37 @@
+const userModel = require("../models/user.model");
+const jwt = require("jsonwebtoken")
+
+
+/**
+ * - user register controller
+ * - POST /api/auth/register
+ * */
+
+
+async function userRegisterController(req, res) {
+    const {email, password, name} = req.body;
+
+    const isExists = await userModel.findOne({
+        email: email
+    })
+
+    if (isExists) {
+        return res.status(422).json({
+            message: "User already exits with email",
+            status: "failed"
+        })
+    }
+
+    const user = await userModel.create({
+        email, password, name
+    })
+
+    const token = jwt.sign({userId: user._id}, process.env.JWT_SECRET, {
+        expiresIn: "3d"
+    })
+
+}
+
+module.exports = {
+    userRegisterController
+}
