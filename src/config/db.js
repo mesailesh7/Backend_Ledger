4c5833d32317eb9ae54cee7a0a2c8d0abe14d422
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2026-03-13 11:08:57 -0600

    Database Connection

---
+++ src/config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+function connectToDB() {
+    mongoose.connect(process.env.MONGO_URI)
+        .then(() => {
+            console.log("Connected to DB");
+        })
+        .catch((err) => {
+            console.error("Error connection to db", err);
+            process.exit(1);
+        })
+}
+
+module.exports = connectToDB;
